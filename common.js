8c85d4299df2464bf067827d87fbc69cd1b3588f
Yasuhiro Kubota <51893971+kubo158@users.noreply.github.com>
2019-10-19 00:39:47 +0900

    Add files via upload

---
+++ common.js
@@ -0,0 +1,110 @@
+'use strict'; {
+
+  //○○○○○○○○○○○○○  ヘッダー始め  ○○○○○○○○○○○○○
+  const header = document.querySelector('header');
+  const divHWrapper = document.createElement('div');
+  divHWrapper.id = 'hWrapper';
+
+
+  const divHInner = document.createElement('div');
+  divHInner.classList.add('h-inner');
+  const notExist = document.createElement('div');
+  notExist.classList.add('not-exist');
+  const pHInner = document.createElement('p');
+  pHInner.textContent = 'こちらは練習用サイトです。店舗は実在しません。';
+
+  const divHTitle = document.createElement('div');
+  divHTitle.classList.add('h-title');
+  const div = document.createElement('div');
+
+  const imgHTitle = document.createElement('img');
+  imgHTitle.id = 'companyLogo';
+  imgHTitle.setAttribute('src', 'img/company_logo.png');
+  imgHTitle.setAttribute('alt', 'logo');
+
+  const h1 = document.createElement('h1');
+  h1.textContent = '🇻🇳ベトナム珈琲 焙煎豆販売所🇻🇳';
+
+  const nav = document.createElement('nav');
+  nav.classList.add('h-nav');
+  const ul = document.createElement('ul');
+
+  const liHome = document.createElement('li');
+  const aHomeTag = document.createElement('a');
+  aHomeTag.href = 'index.html';
+  aHomeTag.textContent = 'ホーム';
+
+  const liVCafe = document.createElement('li');
+  const aVCafeTag = document.createElement('a');
+  aVCafeTag.href = '#';
+  aVCafeTag.textContent = 'ベトナム珈琲のお話';
+
+  const liGoods = document.createElement('li');
+  const aGoods = document.createElement('a');
+  aGoods.href = '#';
+  aGoods.textContent = '商品一覧';
+
+  const liSend = document.createElement('li');
+  const aSend = document.createElement('a');
+  aSend.href = 'shoppingCart.html';
+  aSend.textContent = 'ご購入';
+
+  const liCont = document.createElement('li');
+  liCont.classList.add('h-contact');
+  const aCont = document.createElement('a');
+  aCont.href = 'contact.html';
+  aCont.textContent = 'お問い合わせ';
+
+
+  header.appendChild(divHWrapper);
+
+
+  divHWrapper.appendChild(divHInner);
+  divHInner.appendChild(notExist);
+  notExist.appendChild(pHInner);
+
+  divHInner.appendChild(divHTitle);
+  divHTitle.appendChild(div);
+  div.appendChild(imgHTitle);
+
+  divHTitle.appendChild(h1);
+
+
+  divHWrapper.appendChild(nav);
+  nav.appendChild(ul);
+
+  ul.appendChild(liHome);
+  liHome.appendChild(aHomeTag);
+
+  ul.appendChild(liVCafe);
+  liVCafe.appendChild(aVCafeTag);
+
+  ul.appendChild(liGoods);
+  liGoods.appendChild(aGoods);
+
+  ul.appendChild(liSend);
+  liSend.appendChild(aSend);
+
+  ul.appendChild(liCont);
+  liCont.appendChild(aCont);
+  //○○○○○○○○○○○○○  ヘッダー終り  ○○○○○○○○○○○○○
+
+  //○○○○○○○○○○○○○  フッター始め  ○○○○○○○○○○○○○
+
+  const footer = document.querySelector('footer');
+  const pCopyRight = document.createElement('p');
+  const smallCopyRight = document.createElement('small');
+  smallCopyRight.innerHTML = '&copy; Yasuhiro Kubota 2019 All Right Reserved';
+
+  footer.appendChild(pCopyRight);
+  pCopyRight.appendChild(smallCopyRight);
+
+  //○○○○○○○○○○○○○  フッター終り  ○○○○○○○○○○○○○
+
+
+
+
+
+
+  // 'use strict'の終わり
+}
